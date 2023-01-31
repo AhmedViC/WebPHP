c1c0c832ed537d76487f2d635aba1f882bd00986
AhmedViC <97937459+AhmedViC@users.noreply.github.com>
2023-01-31 09:38:56 +0300

    message for unauthorized Access

--- javaScript/purchaseMsg.js
+++ javaScript/purchaseMsg.js
@@ -1,9 +1,15 @@
 const query  = window.location.search;
 const urlParam = new URLSearchParams(query);
-const msg = urlParam.get('msg');
+console.log('00000')
+const msg = urlParam.get('error');
+console.log(urlParam)
 console.log('hi')
 console.log(msg)
 if(msg=='purchasedone')
 {
    alert('Purchase Completed')
+}
+else if(msg=='unauthorizedAccess')
+{
+   alert('unauthorized Access!')
 }
